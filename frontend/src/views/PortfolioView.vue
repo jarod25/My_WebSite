fbf579d0704a77a5f6d254a2b6916075111f0bb2
jarod25 <kohler.jarod.2004@gmail.com>
2024-02-09 22:54:40 +0100

    ajout page attende du site

--- frontend/src/views/PortfolioView.vue
+++ frontend/src/views/PortfolioView.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="skip-navbar">
     <div class="row">
-      <div v-for="project in projects" :key="project.id" class="col-12">
-        <h1>{{ project.title }}</h1>
+      <div v-for="project in projects" :key="project.id" class="col-xl-12">
+        <h3>{{ project.title }}</h3>
         <div v-if="project.position === 'left'" class="row">
-          <div class="col-2 row">
-            <div v-for="image in project.images" :key="image.id">
-              <div class="col-12 m-2">
-                <img :alt="image.alt" :src="image.url">
-              </div>
+          <div class="col-xl-2 row mt-2">
+            <div class="col-xl-5 mb-3" v-for="image in project.images" :key="image.id">
+              <img :alt="image.alt" :src="image.src" height="100px" width="auto" class="rounded">
             </div>
           </div>
-          <div class="col-10">
-            <p>{{ project.description }}</p>
+          <div class="col-xl-10">
+            <p class="text-justify" v-html="formatDescription(project.description)"></p>
           </div>
         </div>
         <div v-else class="row">
-          <div class="col-10">
-            <p>{{ project.description }}</p>
+          <div class="col-xl-10">
+            <p class="text-justify" v-html="formatDescription(project.description)"></p>
           </div>
-          <div class="col-2 row">
-            <div v-for="image in project.images" :key="image.id">
-              <div class="col-12">
-                <img :alt="image.alt" :src="image.url">
-              </div>
+          <div class="col-xl-2 row mt-2">
+            <div class="col-xl-5 mb-3" v-for="image in project.images" :key="image.id">
+              <img :alt="image.alt" :src="image.src" height="100px" width="auto" class="rounded">
             </div>
           </div>
         </div>
@@ -33,6 +29,7 @@
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -53,6 +50,9 @@ export default {
           .then(response => response.json())
           .then(data => this.projects = data)
     },
+    formatDescription(description) {
+      return description.replace(/\n/g, '<br>');
+    }
   },
   mounted() {
     if (this.$route.path.startsWith('/fr')) {
