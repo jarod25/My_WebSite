ff5eac8a45ac51eaed48705747566b0d41a7b235
jarod25 <kohler.jarod.2004@gmail.com>
2024-03-21 23:47:08 +0100

    Update .gitignore and start backend with express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server start on http://localhost:${port}`);
+});
